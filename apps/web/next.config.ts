6b7e6b6fb6ad4e885a69d123b00818cc87146b93
Avnsmith <avnsmith@gmail.com>
2026-06-08 18:02:27 +0700

    chore: temp log env for verification

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
+console.log("BUILD_TIME_ENV_DB:", process.env.DATABASE_URL);
+console.log("BUILD_TIME_ENV_SRK:", process.env.SUPABASE_SERVICE_ROLE_KEY);
+
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
